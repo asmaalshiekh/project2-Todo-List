e45371a9777e4844739314aad2ad42849376c245
asmaalshiekh <al-shiekh1984@hotmail.com>
2021-12-24 19:29:30 +0300

    finish Put and Delete

--- backend/server.js
+++ backend/server.js
@@ -36,9 +36,49 @@ app.post('/tasks',(req,res)=>{
 })
 
 
+app.delete('/tasks/:id',(req,res)=>{
+    //console.log("37:", req.params.id);    
+
+    Todo.deleteOne({_id:req.params.id},(err, deleteObj)=> { 
+        if(err){
+            console.log("ERROR: " , err);
+        }else{
+            // if ==> else in another statmente
+            deleteObj.deletedCount === 1
+            ? res.json("Delete one this Todo successfully")
+            : res.status(404).json("This Todo is not Found")
+           // console.log(deleteObj);
+           // res.json("Delete one this Todo successfully");
+        }
+   })
+})
+
+
+app.put('/tasks/:id',(req,res)=>{
+    //console.log("37:", req.params.id);    
+    Todo.updateOne(
+        {_id:req.params.id},
+        {title:req.body.newTitle},
+        (err, updateObj)=> { 
+        if(err){
+            console.log("ERROR: " , err);
+            res.status(400).json(err)
+        }else{
+            console.log(updateObj);
+            updateObj.modifiedCount === 1
+            ? res.json("UpDate one Todo successfully")
+            : res.status(404).json("This Todo is not Found")
+           // 
+           // res.json("Delete one this Todo successfully");
+        }
+   })
+})
+
+
+/*
 app.get('/tasks',(req,res)=>{
     //res.json('GET / is Working')
-})
+})*/
 
 app.listen(5000,()=>{
     console.log('SERVER IS WORKING..');
